2891e92153c9ae1386bf124e1145db4660aa8fe5
cheppymk <toso.micevski@gmail.com>
2024-01-03 23:14:57 +0100

    first commit

---
+++ Flights/flights.client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchFlightsComponent } from './search-flights/search-flights.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchFlightsComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
